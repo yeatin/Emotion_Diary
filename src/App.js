52664ac3e5b30b034465f11e91d177edfd311dad
yeatin <lalalatim@yahoo.com.tw>
2022-06-08 22:31:03 +0800

    login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Login from './routes/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [password, setPassword] = useState("");
+  useEffect(() => {
+    setPassword("test");
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login password={password} setPassword={setPassword} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
